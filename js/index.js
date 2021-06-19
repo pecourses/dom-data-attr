638dc5cfab6883971d5949a95509f1a1994b5e2a
Viktoriia <pecourses@gmail.com>
2021-06-19 16:30:49 +0300

    add task

--- js/index.js
+++ js/index.js
@@ -1,16 +1,44 @@
 'use strict';
 
-const btns = document.querySelectorAll('button');
+// const btns = document.querySelectorAll('button');
 
-for (const btn of btns) {
-  btn.addEventListener('click', ({ target }) => {
-    console.dir(target.dataset.host);
-    // console.dir(target.dataset.button - color); // wrong
-    console.dir(target.dataset.buttonColor);
-    // console.dir(target.dataset.firstName); // wrong
-    console.dir(target.dataset.firstname);
-  });
-}
+// for (const btn of btns) {
+//   btn.addEventListener('click', ({ target }) => {
+//     console.dir(target.dataset.host);
+//     // console.dir(target.dataset.button - color); // wrong
+//     console.dir(target.dataset.buttonColor);
+//     // console.dir(target.dataset.firstName); // wrong
+//     console.dir(target.dataset.firstname);
+//   });
+// }
 
 // Создать div с 5 кнопками.
 // Менять цвет дива в зависимости от того, на какую кнопку нажали
+const btns = document.querySelectorAll('button');
+
+const div = document.querySelector('div');
+
+// for (const btn of btns) {
+//   btn.addEventListener(
+//     'click',
+//     ({
+//       target: {
+//         dataset: { color },
+//       },
+//     }) => {
+//       div.style.backgroundColor = color;
+//     }
+//   );
+// }
+
+for (const btn of btns) {
+  btn.addEventListener('click', changeDivColorHandler);
+}
+
+function changeDivColorHandler({
+  target: {
+    dataset: { color },
+  },
+}) {
+  div.style.backgroundColor = color;
+}
